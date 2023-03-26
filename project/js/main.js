57918195a96802342a8e1d7addf1c3220e47cba6
excel <danil_gorokhov@list.ru>
2023-03-26 18:20:43 +0500

    main swiper

---
+++ project/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  // Цикличность
+  loop: true,
+
+  // Автоматическое перелистывание
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
