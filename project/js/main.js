f934d593fdd02e61022df0bb736c9d3904528086
excel <danil_gorokhov@list.ru>
2023-03-27 03:05:52 +0500

    gallery swiper

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper1 = new Swiper('.swiper-container', {
   // Цикличность
   loop: true,
 
@@ -6,5 +6,46 @@ const swiper = new Swiper('.swiper-container', {
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+  },
+});
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".pages__next",
+    prevEl: ".pages__preview",
+  },
+
+  pagination: {
+    el: ".pages__count",
+    type: "fraction",
+  },
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 35,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 35,
+    },
+    320: {
+      slidesPerView: 1,
+    }
   },
 });
